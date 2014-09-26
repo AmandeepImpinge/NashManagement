d34ce76c4fce0acbcf6e08fce80abdf0c87e55f2
AmandeepImpinge <amandeeps@impingeonline.com>
2014-09-26 11:43:07 +0530

    Profile Setup First Modified

--- RacePace/src/com/runtwo/constants/GlobalConstants.java
+++ RacePace/src/com/runtwo/constants/GlobalConstants.java
@@ -1,29 +1,29 @@
 package com.runtwo.constants;
 
 public class GlobalConstants {
-	
+
 	public static final String COMMON_URL = "http://ganga.impingesolutions.com/projects/MobileApps/Patrick/source/apps/index.php/api/"; 
-	
+
 	//Shared Prefs========
 	public static final String PREF_NAME = "racepace_prefs";
 	public static final String USERNAME_SAVE = "username_sp";
 	public static final String PASSWORD_SAVE = "password_sp";
 	//=====================
-	
+
 	//=====================
 	public static final int POST_GALLERY = 120;
 	public static final int POST_CAMERA = 130;
 	//=====================
-	
+
 	public static final String CODE = "code";
 	public static final String MESSAGE = "message";
 	public static final String DATA = "data";
 	public static final String USER_DETAILS = "userDetail";
-	
+
 	//REGISTER VALUES
 	public static final String REGISTERATION_URL = COMMON_URL+"user/register/format/json";
 	//===================
-	
+
 	//COMMON VALUES
 	public static final String USERNAME = "username";
 	public static final String EMAIL = "email";
@@ -31,7 +31,7 @@ public class GlobalConstants {
 	public static final String ACCOUNT_TYPE = "account_type";
 	public static final String DEVICE_TYPE = "device_type";
 	public static final String DEVICE_TOKEN = "device_token";
-	
+
 	public static final String FBUSERID = "fbuser_id";
 	public static final String USER_STATUS = "user_status";
 	public static final String USER_ID = "user_id";
@@ -53,7 +53,9 @@ public class GlobalConstants {
 	public static final String I_FOLLOWING = "ifollowing";
 	public static final String PROFILE_COMPLETION = "profileCompletion";
 	public static final String TOTAL_PROFILE_COMPLETION = "TotalprofileCompletion";
-		
+	public static final String TIMELINE_PIC = "timeline_pic";
+	
+
 	//forlogin data
 	public static final String RUNNING_TEAM = "running_team";
 	public static final String FAV_SHOE = "fav_shoe";
@@ -64,26 +66,28 @@ public class GlobalConstants {
 	public static final String ZIP_CODE = "zip_code";
 	public static final String PHONE_NUM = "phone_num";
 	
+	
+
 	//LOGIN VALUES
 	public static final String LOGIN_URL = COMMON_URL+"authentication/authenticateUser/format/json";
-	
+
 	//=======================
-	
+
 	//FORGOT PASSWORD
 	public static final String FORGOT_PASSWORD_URL = COMMON_URL+"user/forgotPassword/format/json";
 	//=======================
-	
+
 	//GET COMMUNITY==========
 	public static final String GET_COMMUNITY_URL = COMMON_URL+"friend/getCommunity/format/json";
 
 	public static final String CATEGORY_ID = "cat_id";
 	public static final String CATEGORY_NAME = "cat_name";
 	//=======================
-	
+
 	//ADD COMMUNITY==========
 	public static final String ADD_COMMUNITY_URL = COMMON_URL+"friend/addCommunity/format/json";
 	//=======================
-	
+
 	//GET LOGGED OUT=========
 	public static final String LOG_OUT_URL = COMMON_URL+"authentication/updateLoggedInStaus/format/json";
 	//=======================
@@ -100,7 +104,7 @@ public class GlobalConstants {
 	public static final String DASHBOARD_CALORIES = "calories";
 	public static final String DASHBOARD_ADDRESS = "address";
 	//===========================
-	
+
 	//GET FOLLOWERS FEED=========									
 	public static final String GET_FOLLOWER_FEED_URL = COMMON_URL+"friend/getFollowersFeed/format/json";
 	public static final String FEED_DATE = "feed_date";
@@ -113,13 +117,13 @@ public class GlobalConstants {
 	public static final String FEED_PROFILE_IMG = "profile_img";
 	public static final String FEED_POST_PIC = "post_pic";
 	//=======================
-	
+
 	//GET FOLLOWERS == FOLLOWINGS =================
 	public static final String GET_FOLLOWERS_URL = COMMON_URL+"friend/getFollowers/format/json";
 	public static final String FOLLOWERS_MY_FOLLOWER_KEY = "myFollowers";
 	public static final String FOLLOWERS_I_FOLLOWING_KEY = "ifollowing";
 	public static final String FOLLOWERS_TYPE = "type";
-	
+
 	public static final String FOLLOWERS_RP_FRIEND_ID = "rp_friend_id";
 	public static final String FOLLOWERS_FRIEND_ID = "friend_id";
 	public static final String FOLLOWERS_CAT_ID = "cat_id";
@@ -128,7 +132,7 @@ public class GlobalConstants {
 	public static final String FOLLOWERS_PROFILE_IMG = "profile_img";
 	public static final String FOLLOWERS_TOTAL_DIST = "total_distance";
 	//==============================
-	
+
 	//ADD LIKE COMMENT==============
 	public static final String LIKE_COMMENT_URL = COMMON_URL+"friend/addFeedLikeComment/format/json";
 	public static final String LIKE_COMMENT_TYPE = "type";
@@ -136,19 +140,19 @@ public class GlobalConstants {
 	public static final String COMMENT_KEY = "comment";
 	public static final String LIKE_COMMENT_DESC = "comment_desc";
 	//==============================
-	
+
 	//GET LIKE COMMENTS=============
 	public static final String GET_LIKE_COMMENT_URL = COMMON_URL+"friend/getFeedLikesComments/format/json";
 	public static final String RP_COMMENT_ID = "rp_comments_id";
 	//==============================
-	
+
 	//SEARCH FRIENDS AND FOLLOWERS==
 	public static final String SEARCH_FRIENDS_URL = COMMON_URL+"friend/searchFollowersFriends/format/json";
 	public static final String SEARCH_TEXT = "search_text";
 	public static final String FRIEND_KEY = "friend";
 	public static final String FOLLOWER_KEY = "follower";
 	//==============================
-	
+
 	//POST USER FEED================
 	public static final String POST_USER_FEED_URL = COMMON_URL+"friend/createUserFeed/format/json";
 	public static final String POST_IMAGE_NAME = "img_name";
@@ -157,11 +161,16 @@ public class GlobalConstants {
 	public static final String POST_FEED_TYPE = "feed_type";
 	public static final String POST_KEY = "post";
 	//==============================
-	
+
 	//ADD FOLLOWERS=================
 	public static final String ADD_FOLLOWERS_URL = COMMON_URL+"api/friend/addFollower/format/json";
 	public static final String ADD_FOLLOWERS_FRIEND_ID = "friend_id";
 	public static final String ADD_FOLLOWERS_IS_FOLLOWER = "is_follower";
 	//==============================
+
+	//UPDATE USER DETAILS=================
+	public static final String UPDATE_USER_DETAIL_URL = COMMON_URL+"user/updateUser/format/json";
 	
+	//==============================
+
 }
--- RacePace/src/com/runtwo/main/ProfileSetupFirst.java
+++ RacePace/src/com/runtwo/main/ProfileSetupFirst.java
@@ -1,24 +1,280 @@
 package com.runtwo.main;
 
+import java.util.HashMap;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.runtwo.main.R;
+import com.google.android.gms.internal.lf;
+import com.runtwo.constants.GlobalConstants;
+import com.runtwo.webservice.WebServiceHandler;
 
-public class ProfileSetupFirst extends Fragment{
+public class ProfileSetupFirst extends Activity implements OnClickListener /*Fragment*/{	
+
+	EditText eFname, eLname, eEmail, ePass, eCountry, eCity, eAddress, eZip,
+	eRunningTeam, eFavQuote, eDOB, eGender, eWeight, eHeight, eTsize, eShoeSize;
+
+	TextView tSave, tCancel;
+	ProgressDialog mProgressDialog;
+
+	Globals global;
+
+	//View rootView;
+
+	/*@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+		//container = (LinearLayout)inflater.inflate(R.layout.profile_setup_first,null);
+		rootView = inflater.inflate(R.layout.profile_setup_first, container,false);
+
+		getProfileDetails();
+		editProfile();
+
+		return rootView;
+	}*/
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-		container = (LinearLayout)inflater.inflate(R.layout.profile_setup_first,null);
-		
-		
-		
-		
-		return container;
+	protected void onCreate(Bundle savedInstanceState) {		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.profile_setup_first);
+
+		global = (Globals)this.getApplicationContext();
+
+		initializeUI();
+		getProfileDetails();
+	}	
+
+	private void getProfileDetails() {
+		String fName = "";
+		String lName = "";
+
+		Globals global =  (Globals)this.getApplicationContext();	
+
+		HashMap<String, String> userDetails = global.getUserData();
+		if(userDetails.size()!=0) {
+
+			String userName = userDetails.get(GlobalConstants.USERNAME);
+			Log.v("userName", userName);
+			
+			//String password = userDetails.get(GlobalConstants.PASSWORD);
+			//Log.v("password", password);
+
+			String[] parts = userName.split(" ");
+			fName = parts[0]; 
+			lName = parts[1]; 
+
+			eFname.setText(fName);
+			eLname.setText(lName);
+			eEmail.setText(userDetails.get(GlobalConstants.EMAIL));			
+			//ePass.setText(password);
+
+			String country = userDetails.get(GlobalConstants.STATE);			
+			if(country != null) {
+				Log.v("country", country);
+				eCountry.setText(country);
+			}
+
+			String city = userDetails.get(GlobalConstants.CITY);			
+			if(city != null) {
+				Log.v("city", city);
+				eCity.setText(city);
+			}
+
+			String address = userDetails.get(GlobalConstants.ADDRESS);			
+			if(address != null) {
+				Log.v("address", address);
+				eAddress.setText(address);				
+			}
+
+			String zipCode = userDetails.get(GlobalConstants.ZIP_CODE);		
+			if(zipCode != null) {
+				Log.v("zipCode", zipCode);
+				eZip.setText(zipCode);
+			}
+
+			String runningTeam = userDetails.get(GlobalConstants.RUNNING_TEAM);			
+			if(runningTeam != null) {
+				Log.v("runningTeam", runningTeam);
+				eRunningTeam.setText(runningTeam);
+			}
+
+			String favQ = userDetails.get(GlobalConstants.FAV_DIST);
+			if(favQ != null) {
+				Log.v("favQ", favQ);
+				eFavQuote.setText(favQ);
+			}
+
+			String dob = userDetails.get(GlobalConstants.DOB);			
+			if(dob != null) {
+				Log.v("dob", dob);
+				eDOB.setText(dob);
+			}
+
+			String gender = userDetails.get(GlobalConstants.GENDER);			
+			if(gender != null) {
+				Log.v("gender", gender);
+				eGender.setText(gender);
+			}
+
+			String weight = userDetails.get(GlobalConstants.WEIGHT);			
+			if(weight != null) {
+				Log.v("weight", weight);
+				eWeight.setText(weight);
+			}
+
+			String height = userDetails.get(GlobalConstants.HEIGHT);			
+			if(height != null) {
+				Log.v("height", height);
+				eHeight.setText(height);
+			}
+
+			String tSize = userDetails.get(GlobalConstants.TSHIRT_SIZE);			
+			if(tSize != null) {
+				Log.v("tSize", tSize);
+				eTsize.setText(tSize);
+			}
+
+			String shoeSize = userDetails.get(GlobalConstants.SHOE_SIZE);			
+			if(shoeSize != null) {
+				Log.v("shoeSize", shoeSize);
+				eShoeSize.setText(shoeSize);
+			}		
+
+			/*String password = userDetails.get(GlobalConstants.PASSWORD_SAVE);
+			Log.v("password", password);
+			if(password != null) {
+				((EditText)rootView.findViewById(R.id.pass)).setText(password);
+			}*/		
+		}		
+	}	
+
+	public JSONObject saveProfile() {
+
+		String username = (eFname.getText().toString()) +" "+ (eLname.getText().toString());
+		JSONObject obj = new JSONObject();
+
+		try{			
+			obj.accumulate(GlobalConstants.ACCESS_TOKEN,""+global.getAccessToken());	
+			obj.accumulate(GlobalConstants.USERNAME,""+username);			
+			obj.accumulate(GlobalConstants.PASSWORD,""+(ePass.getText().toString()));		
+			//obj.accumulate(GlobalConstants.EMAIL,""+(eEmail.getText().toString()));		
+
+			obj.accumulate("dob",(eDOB.getText().toString()));
+			obj.accumulate("gender",(eGender.getText().toString()));
+			obj.accumulate("weight",(eWeight.getText().toString()));
+			obj.accumulate("height",(eHeight.getText().toString()));
+			obj.accumulate("tshirt_size",(eTsize.getText().toString()));
+			obj.accumulate("shoes_size",(eShoeSize.getText().toString()));			
+			obj.accumulate("goals","");
+			obj.accumulate("motto","");
+			obj.accumulate("img_name","");
+			obj.accumulate("profile_image","");		
+			obj.accumulate("running_team",(eRunningTeam.getText().toString()));
+			obj.accumulate("fav_shoe",(eFavQuote.getText().toString()));
+			obj.accumulate("fav_distance","");
+			obj.accumulate("city",(eCity.getText().toString()));
+			obj.accumulate("state",(eCountry.getText().toString()));
+			obj.accumulate("zip_code",(eZip.getText().toString()));
+			obj.accumulate("phone_num","");
+			obj.accumulate("address",(eAddress.getText().toString()));
+			obj.accumulate("timeline_image","");
+			obj.accumulate("timeline_name","");
+
+		}
+		catch(JSONException j) {
+			j.printStackTrace();
+		}
+		return obj;
+	}	
+
+	private void initializeUI(){	
+
+		eFname = (EditText)findViewById(R.id.first_name);
+		eLname = (EditText)findViewById(R.id.last_name);
+		eEmail = (EditText)findViewById(R.id.email_address);
+		ePass = (EditText)findViewById(R.id.pass);
+		eCountry = (EditText)findViewById(R.id.country_name);
+		eCity = (EditText)findViewById(R.id.city_name);
+		eAddress = (EditText)findViewById(R.id.address);
+		eZip = (EditText)findViewById(R.id.zip_code);
+		eRunningTeam = (EditText)findViewById(R.id.running_team);
+		eFavQuote = (EditText)findViewById(R.id.fav_quote);
+		eDOB = (EditText)findViewById(R.id.dob);
+		eGender = (EditText)findViewById(R.id.sex);
+		eWeight = (EditText)findViewById(R.id.weight);
+		eHeight = (EditText)findViewById(R.id.height);
+		eTsize = (EditText)findViewById(R.id.tShirt_size);
+		eShoeSize = (EditText)findViewById(R.id.shoe_size);		 
+
+		tSave = (TextView)findViewById(R.id.save);
+		tCancel = (TextView)findViewById(R.id.cancel);
+
+		tCancel.setOnClickListener(this);
+		tSave.setOnClickListener(this);
 	}
-	
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+
+		case R.id.cancel:
+			finish();
+			break;
+
+		case R.id.save:
+			//saveProfile();
+			new CallService().execute();
+			break;
+		}		
+	}
+
+	private class CallService extends AsyncTask<String, Integer, String> {
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			new ProgressDialog(ProfileSetupFirst.this);
+			mProgressDialog = ProgressDialog.show(ProfileSetupFirst.this,"","Loading...");
+		}
+
+		@Override
+		protected String doInBackground(String... params) {
+			String result = "true";
+			try {
+				JSONObject jObj = saveProfile();
+				result = WebServiceHandler.updateUserDetails(ProfileSetupFirst.this, jObj);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return result;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			mProgressDialog.dismiss();
+
+			if(result.equalsIgnoreCase("true")){
+				Toast.makeText(ProfileSetupFirst.this,"Profile Updated Succesfully",Toast.LENGTH_SHORT).show();
+				finish();
+			}else if(result.equalsIgnoreCase("false")){
+				Toast.makeText(ProfileSetupFirst.this,""+global.getMessageOfResponse(),Toast.LENGTH_SHORT).show();
+			}else{
+				Toast.makeText(ProfileSetupFirst.this,"Error Connecting to server.",Toast.LENGTH_SHORT).show();
+			}
+		}
+
+	}
 }
